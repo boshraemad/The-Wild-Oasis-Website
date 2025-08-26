728b48e9d8c0f5172e0d7a39d9e85ad3ff55180f
boshraemad <boshraemad2004@icloud.com>
2025-08-27 02:10:36 +0300

    fix : satisfy react/no-unescaped-entities

--- app/_components/ReservationReminder.js
+++ app/_components/ReservationReminder.js
@@ -10,7 +10,7 @@ const {range , resetRange} = useReservation();
   return (
     <div className='fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-full bg-accent-500 text-primary-800 text  font-semibold shadow-xl shadow-slate-900 flex gap-8 items-center'>
       <p>
-        <span>👋</span> Don't forget to reserve your dates <br /> from{' '}
+        <span>👋</span> Don&apos;t forget to reserve your dates <br /> from{' '}
         {format(new Date(range.from), 'MMM dd yyyy')} to{' '}
         {format(new Date(range.to), 'MMM dd yyyy')}
       </p>
